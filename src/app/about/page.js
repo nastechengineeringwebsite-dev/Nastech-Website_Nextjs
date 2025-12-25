292b07091b46586f5c555203d21a6fcae06b5467
Omar Karim <67790370+OmarMahin@users.noreply.github.com>
2025-12-25 14:50:02 +0600

    og added to home and about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,6 +6,23 @@ export const metadata = {
   title: "About | Nas Tech Engineering",
   description:
     "NAS Tech Engineering: a leading engineering solutions & training provider in Bangladesh. We bridge design and production with expert CAD/CAM, CNC, and CAE training.",
+
+  openGraph: {
+    title: "About | Nas Tech Engineering",
+    description:
+      "NAS Tech Engineering: a leading engineering solutions & training provider in Bangladesh. We bridge design and production with expert CAD/CAM, CNC, and CAE training.",
+    url: "https://www.nastechengineeringbd.com/about",
+    siteName: "NAS Tech Engineering",
+    images: ["images/website_preview.webp"],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "About | Nas Tech Engineering",
+    description:
+      "NAS Tech Engineering: a leading engineering solutions & training provider in Bangladesh. We bridge design and production with expert CAD/CAM, CNC, and CAE training.",
+    images: ["images/website_preview.webp"],
+  },
 };
 
 const page = () => {
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,68 +1,98 @@
-import "./globals.css"
+import "./globals.css";
 import Navbar from "./components/Navbar";
-import { Poppins } from "next/font/google"
+import { Poppins } from "next/font/google";
 import Footer from "./components/Footer";
-import { ToastContainer, Slide } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import InitialLoadingAnimation from "./components/InitialLoading";
-import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-poppins",
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export const metadata = {
-	title: "Nas Tech Engineering",
-	description:
-		"NAS Tech Engineering delivers expert CAD/CAM, CNC and DFM training and engineering solutions in Bangladesh, backed by over 10 years of practical industry experience.",
-	metadataBase: new URL("https://www.nastechengineeringbd.com"),
-	alternates: {
-        canonical: './',
-    },
+  title: "NAS Tech Engineering",
+  description:
+    "NAS Tech Engineering delivers expert CAD/CAM, CNC and DFM training and engineering solutions in Bangladesh, backed by over 10 years of practical industry experience.",
+  metadataBase: new URL("https://www.nastechengineeringbd.com"),
+  alternates: {
+    canonical: "./",
+  },
+  openGraph: {
+    title: "NAS Tech Engineering",
+    description:
+      "NAS Tech Engineering delivers expert CAD/CAM, CNC and DFM training and engineering solutions in Bangladesh, backed by over 10 years of practical industry experience.",
+    url: "https://www.nastechengineeringbd.com",
+    siteName: "NAS Tech Engineering",
+    images: ["images/website_preview.webp"],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NAS Tech Engineering",
+    description:
+      "NAS Tech Engineering delivers expert CAD/CAM, CNC and DFM training and engineering solutions in Bangladesh, backed by over 10 years of practical industry experience.",
+    images: ["images/website_preview.webp"],
+  },
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang='en' suppressHydrationWarning>
-			<head>
-				<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-				<link rel='manifest' href='/site.webmanifest' />
-				<link rel='canonical' href='https://www.nastechengineeringbd.com' />
-			</head>
-			
-			<body className={`${poppins.className} antialiased`}>
-				<InitialLoadingAnimation/>
-			<ToastContainer
-					className={"w-[400px] text-center font-semibold text-[16px] duration-700"}
-					position='top-center'
-					autoClose={3000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss={false}
-					draggable={false}
-					pauseOnHover={false}
-					theme='light'
-					transition={Slide}
-					
-				/>
-				<Navbar />
-				
-				<main>{children}</main>
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="canonical" href="https://www.nastechengineeringbd.com" />
+      </head>
 
-				<Footer />
+      <body className={`${poppins.className} antialiased`}>
+        <InitialLoadingAnimation />
+        <ToastContainer
+          className={
+            "w-[400px] text-center font-semibold text-[16px] duration-700"
+          }
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+          theme="light"
+          transition={Slide}
+        />
+        <Navbar />
 
-				<Analytics/>
-				<SpeedInsights />
+        <main>{children}</main>
 
-			</body>
-		</html>
-	)
+        <Footer />
+
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
 }
